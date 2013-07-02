aa28fccea1762be42a6b71c48aa5cce7cc7d1538
Marc Cyr <marcacyr@gmail.com>
2013-07-02 16:15:50 -0700

    Add all files and directories to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'e01d2d2127965353c61650fcb38c73e4706cb9f4618d77e79e2ceed4078dc955fa844f001bd5b359372b9a65e12add5551637e3a6f80ff390833a23361ef97fb'
